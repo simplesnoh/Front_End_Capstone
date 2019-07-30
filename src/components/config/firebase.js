08e3700b41cd8530d2f522ba9835c76cedcfb12d
Sydney Noh <43580474+simplesnoh@users.noreply.github.com>
2019-07-30 15:37:37 -0500

    Delete firebase.js

--- src/components/config/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCNKtkdwx-N8phSE7NcIeb6X5yYB9EyEZ8",
-    authDomain: "tasktue.firebaseapp.com",
-    databaseURL: "https://tasktue.firebaseio.com",
-    projectId: "tasktue",
-    storageBucket: "tasktue.appspot.com",
-    messagingSenderId: "766504630851",
-    appId: "1:766504630851:web:452b8bb0c346baf7"
-};
